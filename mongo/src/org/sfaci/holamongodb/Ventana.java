71b373b7df2423a290084238f97b6dcbbf48e32e
Patricia Sanz Escurin <patricia.sanz.escurin@gmail.com>
2022-11-21 10:40:55 +0100

    Cambios por concesionario

--- mongo/src/org/sfaci/holamongodb/Ventana.java
+++ mongo/src/org/sfaci/holamongodb/Ventana.java
@@ -4,12 +4,7 @@ import com.toedter.calendar.JDateChooser;
 
 import javax.swing.*;
 
-/**
- * Formulario de la aplicación
- *
- * @author Santiago Faci
- * @version curso 2015-2016
- */
+
 public class Ventana {
     public JPanel panel1;
     public JTextField tfAutor;
